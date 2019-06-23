8dcd4b52d8105dee6e7acb79d2e1c6c8dadaa215
Harry Cui <harry.cui@yahoo.ca>
2019-06-23 13:28:01 -0400

    Updated calendar code

---
+++ src/usr-select-multiple.js
@@ -0,0 +1,26 @@
+import { css, html, LitElement } from 'lit-element';
+import { repeat } from 'lit-html/directives/repeat';
+
+export class UsrSelectMultiple extends LitElement {
+  static get styles() {
+    return [
+
+    ];
+  }
+
+  static get properties() {
+    return {
+
+    };
+  }
+
+  constructor() {
+    super();
+  }
+
+  render() {
+    
+  }
+}
+
+window.customElements.define('usr-select-multiple', UsrSelectMultiple);
